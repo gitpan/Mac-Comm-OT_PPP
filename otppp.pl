4d105c77e546488e8b29cc0e7a9c98f3104e2b10
Chris Nandor <cnandor@cpan.org>
1998-01-06 15:50:02 +0000

    Import of CNANDOR/Mac-Comm-OT_PPP-1.20 from CPAN.
    gitpan-cpan-distribution: Mac-Comm-OT_PPP
    gitpan-cpan-version:      1.20
    gitpan-cpan-path:         CNANDOR/Mac-Comm-OT_PPP-1.20.tar.gz
    gitpan-cpan-author:       CNANDOR
    gitpan-cpan-maturity:     released

--- otppp.pl
+++ otppp.pl
@@ -1,18 +1,21 @@
-#!perl
+#!perl -wl
+use strict;
 use Mac::Comm::OT_PPP;
+use Mac::Files;
+my($ppp, $user, $pass, $adrs, $otstat);
+
 $ppp = new Mac::Comm::OT_PPP;
-$user = 'chrisn';
+$user = 'pudge';
 $pass = 'password';
 $adrs = '5551212';
 
-$ppp->PPPdisconnect;
+$ppp->PPPdisconnect();
 $ppp->PPPconnect($user,$pass,$adrs);
 
-$hash = $ppp->PPPstatus;
-foreach $key (keys %{$hash}) {
-    print "$key: $$hash{$key}\n";
-}
+$otstat = $ppp->PPPstatus();
+print map "$_: $$otstat{$_}\n", keys %$otstat;
 
-$ppp->PPPsavelog('PowerPudge:Desktop Folder:temp');
+print "Please wait while log is saved to desktop ...\n";
+$ppp->PPPsavelog(FindFolder(kOnSystemDisk(),kDesktopFolderType()).':ppplog.txt');
 
-print "Done.\n\n";
+print "Done.";
